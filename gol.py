b18a2f51ef056d0af2493f61ddf2a1306f4b0c6b
hanksterhan <henry.s.han@hotmail.com>
2017-04-29 17:02:09 -0400

    Started parsing the command line
    sys.stdout.flush()  is the equivalent to fflush(stdout) in C
    sys.argv is the equivalent to argv in C

--- gol.py
+++ gol.py
@@ -1,4 +1,23 @@
-import threads
+import threads, sys
+
+# this function parses the command line, making sure all the command line arguments are right
+# returns a list
+def parseCommandLine():
+    if len(sys.argv) is not 6:
+        print "Please input commands in the following format:\n"
+        print "gol.py file.txt printFlag numThreads rowColFlag printConfigFlag\n"
+        print "where printFlag, rowColFlag and printConfigFlag are all 0 or 1"
+        sys.stdout.flush() # python equivalent to fflush(stdout) in C
+        sys.exit(0)
+    elif sys.argv[2] is not in [0,1]:
+        print "The second argument must be a 0 or 1\n"
+        print "0 will not print the game board after each iteration\n"
+        print "1 will print the game board after each iteration"
+    elif sys.argv[3]
+
+
+
+
 
 def main():
 
